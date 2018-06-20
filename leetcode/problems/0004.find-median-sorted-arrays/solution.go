01188ec6172c4c92af5bb196d16d12d50266c920
Shine Xia <shine.xgh@gmail.com>
2018-06-20 11:33:44 +0800

    add leetcode problem 0004

--- leetcode/problems/0004.find-median-sorted-arrays/solution.go
+++ leetcode/problems/0004.find-median-sorted-arrays/solution.go
@@ -3,25 +3,66 @@ package _004_find_median_sorted_arrays
 // https://leetcode.com/problems/median-of-two-sorted-arrays/description/
 
 // Time: O(log(m + n))
-func findMedianSortedArrays(n1 []int, n2 []int) float64 {
-	N1 := len(n1)
-	if N1 == 0 {
-		return findMedianSortedArray(n2, len(n2))
+func findMedianSortedArrays(A []int, B []int) float64 {
+	m := len(A)
+	n := len(B)
+	if m > n {
+		// 保证 m <= n
+		m, n = n, m
+		A, B = B, A
 	}
-	N2 := len(n2)
-	if N2 == 0 {
-		return findMedianSortedArray(n1, N1)
+	var (
+		iMin    = 0
+		iMax    = m
+		halfLen = (m + n + 1) / 2
+	)
+	for iMin <= iMax {
+		i := (iMin + iMax) / 2
+		j := halfLen - i
+		if i < iMax && B[j-1] > A[i] {
+			// i is too small
+			iMin = iMin + 1
+		} else if i > iMin && A[i-1] > B[j] {
+			// i is too big
+			iMax = iMax - 1
+		} else {
+			// i is perfect
+			maxLeft := 0
+			if i == 0 {
+				maxLeft = B[j-1]
+			} else if j == 0 {
+				maxLeft = A[i-1]
+			} else {
+				maxLeft = max(A[i-1], B[j-1])
+			}
+			if (m+n)&0x1 == 1 {
+				// 总数为奇数
+				return float64(maxLeft)
+			}
+			minRight := 0
+			if i == m {
+				minRight = B[j]
+			} else if j == n {
+				minRight = A[i]
+			} else {
+				minRight = min(B[j], A[i])
+			}
+			return float64(maxLeft+minRight) * 0.5
+		}
 	}
-	p1 := n1[(N1 - 1) >> 1]
-	p2 := n2[(N2 + 1) >> 1]
+	return 0.0
 }
 
-func findMedianSortedArray(nums []int, N int) (out float64) {
-	p1 := (N - 1) >> 1
-	if N & 0x1 == 0 && N > 1 {
-		out = float64(nums[p1] + nums[p1 + 1]) / 2.0
-	} else if N > 0 {
-		out = float64(nums[p1])
+func max(a, b int) int {
+	if a > b {
+		return a
 	}
-	return
+	return b
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
 }
--- leetcode/problems/0004.find-median-sorted-arrays/solution_test.go
+++ leetcode/problems/0004.find-median-sorted-arrays/solution_test.go
@@ -1,2 +1,26 @@
 package _004_find_median_sorted_arrays
 
+import (
+	"testing"
+	"math"
+)
+
+func TestFindMedianSortedArrays(t *testing.T) {
+	tests := []struct {
+		a      []int
+		b      []int
+		expect float64
+	} {
+		{a: []int{}, b: []int{1}, expect: 1},
+		{a: []int{1}, b: []int{2}, expect: 1.5},
+		{a: []int{1}, b: []int{2, 3}, expect: 2},
+		{a: []int{1, 2}, b: []int{3, 4}, expect: 2.5},
+		{a: []int{4, 5}, b: []int{1, 2, 3}, expect: 3},
+	}
+
+	for _, test := range tests {
+		if o := findMedianSortedArrays(test.a, test.b); math.Abs(o - test.expect) > 0.000001 {
+			t.Errorf("a=%v, b=%v, out=%f, expect=%f", test.a, test.b, o, test.expect)
+		}
+	}
+}
