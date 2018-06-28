1312763d6822765a2cfcbc5868e98a9736a72da4
shine xia <shine.xgh@gmail.com>
2018-06-28 09:06:12 +0800

    add books/dsaa/gcd.c

---
+++ books/dsaa/ch01/gcd.c
@@ -0,0 +1,11 @@
+
+unsigned int GCD(unsigned int M, unsigned int N) {
+  unsigned int Rem;
+  while (N > 0) {
+    /* C语言中`%`是取余操作，不是取模 */
+    Rem = M % N;
+    M = N;
+    N = Rem;
+  }
+  return M;
+}
