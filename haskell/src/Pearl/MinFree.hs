876365529475ecdc1bf525dd319b77f1d89f7633
shine xia <shine.xgh@gmail.com>
2018-07-13 19:03:50 +0800

    add support for haskell

---
+++ haskell/src/Pearl/MinFree.hs
@@ -0,0 +1,11 @@
+module Pearl.MinFree where
+
+minfree :: [Integer] -> Integer
+minfree xs = head ([0 ..] \\ xs)
+  where
+    (\\) :: Eq a => [a] -> [a] -> [a]
+    us \\ vs = filter (fn vs) us
+
+    fn :: Eq a => [a] -> a -> Bool
+    fn vs x = notElem x vs
+
