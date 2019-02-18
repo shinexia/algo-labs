cc037c8b37ec58cf4125c2dc612a9e96e188e349
Shine Xia <guanghui.xia@rokid.com>
2019-02-18 09:29:26 +0800

    update 0009/palindrome_number

--- leetcode/problems/0009_palindrome_number/palindrome_number.h
+++ leetcode/problems/0009_palindrome_number/palindrome_number.h
@@ -5,23 +5,8 @@
 #ifndef ALGORITHM_LABS_PALINDROME_NUMBER_H
 #define ALGORITHM_LABS_PALINDROME_NUMBER_H
 
-class Solution {
-public:
-    bool isPalindrome(int x) {
-        if (x == 0) {
-            return true;
-        }
-        if (x < 0 || (x % 10 == 0)) {
-            return false;
-        }
-        int rev = 0;
-        while (x > rev) {
-            rev = rev * 10 + x % 10;
-            x = x / 10;
-        }
-        return x == rev || x == rev / 10;
-    }
-};
+
+bool isPalindrome(int x);
 
 
 #endif //ALGORITHM_LABS_PALINDROME_NUMBER_H
--- leetcode/problems/0009_palindrome_number/palindrome_number_test.cc
+++ leetcode/problems/0009_palindrome_number/palindrome_number_test.cc
@@ -15,25 +15,23 @@ typedef struct Case_ {
 
 TEST(TestPalindromeNumber, simple) {
     Case cases[] = {
-            {-1, false},
-            {0, true},
-            {1, true},
-            {10, false},
-            {121, true},
-            {-121, false},
-            {22, true},
-            {21, false},
-            {123, false},
+            {-1,         false},
+            {0,          true},
+            {1,          true},
+            {10,         false},
+            {121,        true},
+            {-121,       false},
+            {22,         true},
+            {21,         false},
+            {123,        false},
             {1073773701, true},
     };
 
     int N = sizeof(cases) / sizeof(Case);
 
-    Solution solution;
-
     for (int i = 0; i < N; i++) {
         const Case *c = &cases[i];
-        bool ret = solution.isPalindrome(c->x);
+        bool ret = isPalindrome(c->x);
         EXPECT_EQ(ret, c->expect) << "x=" << c->x << ", out=" << ret << ", expect=" << c->expect;
     }
 }
